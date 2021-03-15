6141427f5003a80de9b4ec18f5d3a16dc4177c1e
kabir-webDev <kabir.webdev@gmail.com>
2021-03-16 02:20:28 +0600

    Boom

--- src/App.js
+++ src/App.js
@@ -2,13 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      {/* <h2>Shikder Pharmacy</h2>
-      <h1>Coming Soon...</h1> */}
-      comming...
-    </div>
-  );
+  return <div className="App">Learn React</div>;
 }
 
 export default App;
