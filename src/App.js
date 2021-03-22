eb6e65894ae39cb790dad3e2a26096b107aebe28
kabir-webDev <kabir.webdev@gmail.com>
2021-03-22 22:52:50 +0600

    Auth

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import React from "react";
-import MatModal from "./components/MatModal";
 
 function App() {
   return (
     <div className="App">
       <h1>Walah</h1>
-      <MatModal />
     </div>
   );
 }
