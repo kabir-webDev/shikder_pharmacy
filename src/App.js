aa7171036c7375c9826abc6058eb2325ab9d205a
kabir-webDev <kabir.webdev@gmail.com>
2021-04-15 20:21:09 +0600

    Firebase CRUD

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 import React from "react";
+import FireData from "./components/FireData";
+import AddToFire from "./components/AddToFire";
+import Inputss from "./components/Inputss";
 
 function App() {
   return (
     <div className="App">
       <h1>Walah</h1>
+      {/* <AddToFire /> */}
+      <Inputss />
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,28 @@
+// import firebase from "firebase/app";
+// import "firebase/firestore";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC2DSX1G1h5l1Q9uvfTBTUPOWLaWLOvc0Q",
+//   authDomain: "sikder-pharma.firebaseapp.com",
+//   projectId: "sikder-pharma",
+//   storageBucket: "sikder-pharma.appspot.com",
+//   messagingSenderId: "17385553540",
+//   appId: "1:17385553540:web:8eaeec21448c3c989eccd1",
+// };
+
+// firebase.initializeApp(firebaseConfig);
+
+// export default firebase;
+
 import firebase from "firebase";
 
 var firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyBn_aCAKcgpMqYLgRAV9qwUM9VdgzUdXfo",
-  authDomain: "firbase-learn-360.firebaseapp.com",
-  databaseURL: "https://firbase-learn-360-default-rtdb.firebaseio.com",
-  projectId: "firbase-learn-360",
-  storageBucket: "firbase-learn-360.appspot.com",
-  messagingSenderId: "196987928754",
-  appId: "1:196987928754:web:d41acbd8444bbaa47d49ab",
+  apiKey: "AIzaSyC2DSX1G1h5l1Q9uvfTBTUPOWLaWLOvc0Q",
+  authDomain: "sikder-pharma.firebaseapp.com",
+  projectId: "sikder-pharma",
+  storageBucket: "sikder-pharma.appspot.com",
+  messagingSenderId: "17385553540",
+  appId: "1:17385553540:web:8eaeec21448c3c989eccd1",
 });
 
 const db = firebaseApp.firestore();
