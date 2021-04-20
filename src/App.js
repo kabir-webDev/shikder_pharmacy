7fd5f0c560d6d0ca0aeb7a2d2a6771d749c7c2c3
kabir-webDev <kabir.webdev@gmail.com>
2021-04-21 00:57:38 +0600

    Formik-Pro

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import AddToFire from "./components/AddToFire";
 import Inputss from "./components/Inputss";
 import TestInput from "./components/TestInput";
 import FormikPracOne from "./components/Formik/FormikPracOne";
+import FormikPracTwo from "./components/Formik/FormPracTwo";
 
 function App() {
   return (
     <div className="App">
-      <FormikPracOne />
+      {/* <FormikPracOne /> */}
+      <FormikPracTwo />
     </div>
   );
 }
