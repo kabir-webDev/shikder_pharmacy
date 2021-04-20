bb186e89591605e36aaf05370be0948e6e06842c
kabir-webDev <kabir.webdev@gmail.com>
2021-04-20 15:35:31 +0600

    Input Solved

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import React from "react";
 import FireData from "./components/FireData";
 import AddToFire from "./components/AddToFire";
 import Inputss from "./components/Inputss";
+import TestInput from "./components/TestInput";
 
 function App() {
   return (
     <div className="App">
-      <h1>Walah</h1>
-      {/* <AddToFire /> */}
-      <Inputss />
+      <TestInput />
     </div>
   );
 }
